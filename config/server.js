ff25b43ef697a9818c38ea27a7ede52c7683e588
Chanice Irungu <chaniceashley@gmail.com>
2023-12-13 17:00:55 +0300

    proxy for web auth url

--- config/server.js
+++ config/server.js
@@ -3,6 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int("PORT", 1337),
   app: {
     keys: env.array("APP_KEYS"),
+    proxy:true,
   },
   url: "https://quest-postgress.onrender.com",
   webhooks: {
